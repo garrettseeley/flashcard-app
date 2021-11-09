738eccc21c23c5a3f6431e725ae8f9000349df57
Garrett <seeleysurfer01@gmail.com>
2021-11-09 11:35:22 -0600

    Study component and card component finished

---
+++ src/Layout/Cards/Card.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import { useHistory, Link, useParams } from "react-router-dom";
+
+export default function Card({ cards = [] }) {
+  const [currentCard, setCurrentCard] = useState(0);
+  const [cardState, setCardState] = useState(true);
+  const history = useHistory();
+  const { deckId } = useParams();
+
+  if (cards.length <= 2) {
+      return (
+          <div>
+              <h3>Not enough cards.</h3>
+              <p>You need at least 3 cards to study.  There are {cards.length} cards in this deck.</p>
+              <Link to={`/decks/${deckId}/cards/new`} className="btn btn-primary"><span className="oi oi-plus mr-2"></span>Add Cards</Link>
+          </div>
+      )
+  }
+
+  //Determines which side of the card is showing
+  const cardInfo = cardState
+    ? cards[currentCard].front
+    : cards[currentCard].back;
+
+  //handles the on flip event
+  function handleFlip(event) {
+    event.preventDefault();
+    return setCardState(!cardState);
+  }
+  //handle the next event
+  function handleNext(event) {
+    event.preventDefault();
+    setCardState(true);
+    if (currentCard + 1 < cards.length) {
+      setCurrentCard(currentCard + 1);
+    } else {
+      return window.confirm("Restart cards?")
+        ? history.go(0)
+        : history.push("/");
+    }
+  }
+
+  return (
+    <div className="card">
+      <div className="card-body">
+        <div className="card-title">
+          <h4>Card {currentCard + 1} of {cards.length}</h4>
+        </div>
+        <div className="card-text">
+          <p>{cardInfo}</p>
+        </div>
+        <div>
+          <button
+            onClick={(event) => handleFlip(event)}
+            className="btn btn-secondary mr-2"
+          >
+            Flip
+          </button>
+          <button
+            onClick={(event) => handleNext(event)}
+            className="btn btn-primary"
+          >
+            Next
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/Layout/Home/Study.js
+++ src/Layout/Home/Study.js
@@ -1,7 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams, Link } from "react-router-dom";
+import { readDeck } from "../../utils/api";
+import Card from "../Cards/Card";
 
 export default function Study() {
-    return (
-        <p>This is a test</p>
-    )
+  const { deckId } = useParams();
+  const [deck, setDeck] = useState([]);
+
+  useEffect(() => {
+    const abortController = new AbortController();
+    async function singleDeck() {
+      const response = await readDeck(deckId, abortController.signal);
+      setDeck(response);
+    }
+    singleDeck();
+  }, [deckId]);
+
+  return (
+    <div>
+      <div className="row">
+        <nav aria-label="breadcrumb" className="col">
+          <ol class="breadcrumb">
+            <li class="breadcrumb-item">
+              <Link to="/">
+                <span className="oi oi-home mr-1"></span>Home
+              </Link>
+            </li>
+            <li class="breadcrumb-item">
+              <Link to={`/decks/${deckId}`}>{deck.name}</Link>
+            </li>
+            <li class="breadcrumb-item active" aria-current="page">
+              Study
+            </li>
+          </ol>
+        </nav>
+      </div>
+      <div>
+        <h2>Study: {deck.name}</h2>
+      </div>
+      <div>
+          <Card cards={deck.cards} />
+      </div>
+    </div>
+  );
 }
